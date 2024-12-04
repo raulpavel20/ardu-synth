01aea0a7f72ea3fe45ffa0dbe84617a34fe4f8aa
Pavel Raul <raul.pavel20@gmail.com>
2024-12-04 20:17:02 +0200

    sync screen to sound

--- VCO/VCO.ino
+++ VCO/VCO.ino
@@ -2,34 +2,67 @@
 #include <Mozzi.h>
 #include <Oscil.h>
 #include <tables/sin2048_int8.h>
+#include <tables/saw2048_int8.h>
 #include <tables/triangle2048_int8.h>
-#include <tables/square_no_alias_2048_int8.h>
+#include <tables/square_analogue512_int8.h>
 
-Oscil <SIN2048_NUM_CELLS, MOZZI_AUDIO_RATE> sinWave(SIN2048_DATA);
-Oscil <TRIANGLE2048_NUM_CELLS, MOZZI_AUDIO_RATE> triangleWave(SIN2048_DATA);
-Oscil <SQUARE_NO_ALIAS_2048_NUM_CELLS, MOZZI_AUDIO_RATE> sqareWave(SQUARE_NO_ALIAS_2048_DATA);
+Oscil<SIN2048_NUM_CELLS, MOZZI_AUDIO_RATE> sinWave(SIN2048_DATA);
+Oscil<TRIANGLE2048_NUM_CELLS, MOZZI_AUDIO_RATE> triangleWave(TRIANGLE2048_DATA);
+Oscil<SQUARE_ANALOGUE512_NUM_CELLS, MOZZI_AUDIO_RATE> sqareWave(SQUARE_ANALOGUE512_DATA);
+Oscil<SAW2048_NUM_CELLS, MOZZI_AUDIO_RATE> sawWave(SAW2048_DATA);
 
-void setup(){
+enum oscTypes { SIN,
+                SQR,
+                TRI,
+                SAW };
+
+enum oscTypes oscType;
+String dataInput = "";
+int readAux;
+
+void setup() {
   Serial.begin(9600);
 
   sinWave.setFreq(440);
   triangleWave.setFreq(440);
   sqareWave.setFreq(440);
+  sawWave.setFreq(440);
 
   startMozzi();
 }
 
-void updateControl(){
+void updateControl() {
   // int analogValue = analogRead(ENV_INPUT_PIN);
   // envelopeValue = (float)analogValue / 1023.0;
 }
 
-AudioOutput updateAudio(){
-  int scaledOutput = (int)(sinWave.next() * 1);
+AudioOutput updateAudio() {
+  int scaledOutput = (int)(getOutput1());
   return MonoOutput::from8Bit(scaledOutput);
 }
 
-
-void loop(){
+void loop() {
   audioHook();
+  readData();
+}
+
+int8_t getOutput1() {
+  switch (oscType) {
+    case SIN:
+      return sinWave.next();
+    case SQR:
+      return sqareWave.next();
+    case TRI:
+      return triangleWave.next();
+    case SAW:
+      return sawWave.next();
+  }
+}
+
+void readData() {
+  if (Serial.available()) {
+    dataInput = Serial.readStringUntil('\n');
+    sscanf(dataInput.c_str(), "%d", &readAux);
+    oscType = readAux;
+  }
 }
--- VCO_SCREEN/VCO_SCREEN.ino
+++ VCO_SCREEN/VCO_SCREEN.ino
@@ -19,40 +19,17 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Encoder mainEnc(ENCODER_PIN_A, ENCODER_PIN_B);
 
-enum scrollActions { OSC_TYPE,
-                     OSC_INDEX };
 enum oscTypes { SIN,
                 SQR,
-                TRI };
+                TRI,
+                SAW };
 
-typedef struct {
-  double x;
-  double y;
-} Point;
-typedef struct {
-  Point a;
-  Point b;
-  Point c;
-} Triangle;
-
-Triangle triangle1 = {
-  .a = { 33.00, 20.00 },
-  .b = { 43.39, 38.00 },
-  .c = { 22.61, 38.00 }
-};
-
-Triangle triangle2 = {
-  .a = { 95.00, 20.00 },
-  .b = { 105.39, 38.00 },
-  .c = { 84.61, 38.00 }
-};
-enum scrollActions crtAction = OSC_INDEX;
-enum oscTypes osc0Type = TRI;
-enum oscTypes osc1Type = TRI;
-int selectedOsc = 0;
+enum oscTypes oscType = SIN;
+int centerX = SCREEN_WIDTH / 2;
+int centerY = SCREEN_HEIGHT / 2;
 
 enum oscTypes incrementOsc(enum oscTypes currentOsc) {
-  if (currentOsc == TRI) {
+  if (currentOsc == SAW) {
     return SIN;
   }
   return static_cast<oscTypes>(static_cast<int>(currentOsc) + 1);
@@ -60,126 +37,98 @@ enum oscTypes incrementOsc(enum oscTypes currentOsc) {
 
 enum oscTypes decrementOsc(enum oscTypes currentOsc) {
   if (currentOsc == SIN) {
-    return TRI;
+    return SAW;
   }
   return static_cast<oscTypes>(static_cast<int>(currentOsc) - 1);
 }
 
 void setup() {
   Serial.begin(9600);
+
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
-    Serial.println("SSD1306 allocation failed");
     for (;;)
       ;
   }
   delay(100);  // let the display initialize
-  Serial.println("SSD1306 initialized");
 
   refreshScreen();
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    if (crtAction == OSC_TYPE) {
-      crtAction = OSC_INDEX;
-    } else {
-      crtAction = OSC_TYPE;
-    }
-    refreshScreen();
-    delay(300);
-  }
-
   long newEncPosition = mainEnc.readAndReset();
   if (newEncPosition == 0) {
     return;
   }
 
-  if (crtAction == OSC_INDEX) {
-    if (newEncPosition == 1) {
-      if (selectedOsc == 0) {
-        selectedOsc = 1;
-        refreshScreen();
-      }
-    } else if (newEncPosition == -1) {
-      selectedOsc = 0;
-      refreshScreen();
-    }
-  } else {
-    if (selectedOsc == 0) {
-      osc0Type = newEncPosition == 1 ? incrementOsc(osc0Type) : newEncPosition == -1 ? decrementOsc(osc0Type)
-                                                                                     : osc0Type;
-    } else {
-      osc1Type = newEncPosition == 1 ? incrementOsc(osc1Type) : newEncPosition == -1 ? decrementOsc(osc1Type)
-                                                                                     : osc1Type;
-    }
-    refreshScreen();
-    delay(50);
-  }
+  oscType = newEncPosition == 1 ? incrementOsc(oscType) : newEncPosition == -1 ? decrementOsc(oscType)
+                                                                               : oscType;
+  refreshScreen();
+  Serial.println(oscType);
+  delay(100);
 }
-
-const int rectWidth = (SCREEN_WIDTH - 3 * PADDING) / 2;
-const int rectHeight = SCREEN_HEIGHT - 2 * PADDING;
-const int symbolWidth = 12;
-int rect1CenterX = PADDING + rectWidth / 2;
-int rect2CenterX = PADDING * 2 + rectWidth + rectWidth / 2;
-int rectCenterY = PADDING + rectHeight / 2;
 
 void refreshScreen() {
   display.clearDisplay();
-
-  if (selectedOsc == 0) {
-    if (crtAction == OSC_INDEX) {
-      display.fillRect(PADDING, PADDING, rectWidth, rectHeight, SSD1306_WHITE);
-      display.fillRect(PADDING + 4, PADDING + 4, rectWidth - 8, rectHeight - 8, SSD1306_BLACK);
-    } else {
-      display.fillRect(PADDING, PADDING, rectWidth, rectHeight, SSD1306_WHITE);
-    }
-    display.drawRect(PADDING * 2 + rectWidth, PADDING, rectWidth, rectHeight, SSD1306_WHITE);
-  } else {
-    display.drawRect(PADDING, PADDING, rectWidth, rectHeight, SSD1306_WHITE);
-    if (crtAction == OSC_INDEX) {
-      display.fillRect(PADDING * 2 + rectWidth, PADDING, rectWidth, rectHeight, SSD1306_WHITE);
-      display.fillRect(PADDING * 2 + rectWidth + 4, PADDING + 4, rectWidth - 8, rectHeight - 8, SSD1306_BLACK);
-    } else {
-      display.fillRect(PADDING * 2 + rectWidth, PADDING, rectWidth, rectHeight, SSD1306_WHITE);
-    }
-  }
-
-  if (crtAction == OSC_INDEX) {
-    drawOscTypeSymbol(osc0Type, rect1CenterX, true, 0);
-    drawOscTypeSymbol(osc1Type, rect2CenterX, true, 1);
-  } else {
-    if (selectedOsc == 0) {
-      drawOscTypeSymbol(osc0Type, rect1CenterX, false, 0);
-      drawOscTypeSymbol(osc1Type, rect2CenterX, true, 1);
-    } else {
-      drawOscTypeSymbol(osc0Type, rect1CenterX, true, 0);
-      drawOscTypeSymbol(osc1Type, rect2CenterX, false, 1);
-    }
-  }
-
+  drawOscTypeSymbol();
+  drawVolumeBar();
   display.display();
 }
 
-void drawOscTypeSymbol(enum oscTypes type, int center, bool filled, int index) {
-  int color = filled ? SSD1306_WHITE : SSD1306_BLACK;
-  switch (type) {
+void drawOscTypeSymbol() {
+  int borderWidth = SCREEN_WIDTH - 32;
+  int borderHeight = SCREEN_HEIGHT - 2 * PADDING;
+
+  display.drawRect(PADDING, PADDING, borderWidth, borderHeight, SSD1306_WHITE);
+
+  int centerX = PADDING + borderWidth / 2;
+  int centerY = PADDING + borderHeight / 2;
+  int symbolWidth = borderWidth - 20;
+  int symbolHeight = borderHeight - 20;
+
+  switch (oscType) {
     case SIN:
-      display.fillCircle(center, rectCenterY, symbolWidth, color);
-      break;
-    case SQR:
-      display.fillRect(center - symbolWidth, rectCenterY - symbolWidth, symbolWidth * 2, symbolWidth * 2, color);
-      break;
-    case TRI:
-      if (index == 0) {
-        display.fillTriangle(triangle1.a.x, triangle1.a.y, triangle1.b.x, triangle1.b.y, triangle1.c.x, triangle1.c.y, color);
-      } else {
-        display.fillTriangle(triangle2.a.x, triangle2.a.y, triangle2.b.x, triangle2.b.y, triangle2.c.x, triangle2.c.y, color);
+      for (int x = -symbolWidth / 2; x <= symbolWidth / 2; x++) {
+        int y = (int)(sin(x * 0.1) * symbolHeight / 2);
+        display.drawPixel(centerX + x, centerY - y, SSD1306_WHITE);
       }
       break;
+
+    case SQR:
+      display.drawLine(centerX - symbolWidth / 2, centerY - symbolHeight / 2, centerX, centerY - symbolHeight / 2, SSD1306_WHITE);          // Top line
+      display.drawLine(centerX, centerY - symbolHeight / 2, centerX, centerY + symbolHeight / 2, SSD1306_WHITE);                            // Right edge
+      display.drawLine(centerX, centerY + symbolHeight / 2, centerX + symbolWidth / 2, centerY + symbolHeight / 2, SSD1306_WHITE);          // Bottom line
+      display.drawLine(centerX + symbolWidth / 2, centerY + symbolHeight / 2, centerX + symbolWidth / 2, centerY - symbolHeight / 2, SSD1306_WHITE);
+      break;
+
+    case TRI:
+      display.drawLine(centerX - symbolWidth / 2, centerY + symbolHeight / 2, centerX, centerY - symbolHeight / 2, SSD1306_WHITE);  // Upward slope
+      display.drawLine(centerX, centerY - symbolHeight / 2, centerX + symbolWidth / 2, centerY + symbolHeight / 2, SSD1306_WHITE);  // Downward slope
+      break;
+
+    case SAW:
+      int step = symbolWidth / 2;
+      for (int x = -symbolWidth / 2; x < symbolWidth / 2; x += step) {
+        display.drawLine(centerX + x, centerY + symbolHeight / 2, centerX + x + step, centerY - symbolHeight / 2, SSD1306_WHITE);         // Upward slope
+        display.drawLine(centerX + x + step, centerY - symbolHeight / 2, centerX + x + step, centerY + symbolHeight / 2, SSD1306_WHITE);  // Vertical drop
+      }
+      break;
+
     default:
       break;
   }
+}
+
+void drawVolumeBar() {
+  int barX = SCREEN_WIDTH - 20;
+  int barY = PADDING;
+  int barWidth = 16;
+  int barHeight = SCREEN_HEIGHT - 2 * PADDING;
+  int volumeLevel = 50;
+
+  display.drawRect(barX, barY, barWidth, barHeight, SSD1306_WHITE);
+
+  int filledHeight = (volumeLevel * barHeight) / 100;
+  display.fillRect(barX + 2, barY + barHeight - filledHeight, barWidth - 4, filledHeight, SSD1306_WHITE);
 }
